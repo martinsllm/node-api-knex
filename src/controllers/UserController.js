0785f2b3a16189cbc5203a1fd9d9eb7e8861dee8
lucas <lucaslm2010@gmail.com>
2022-06-17 21:01:01 -0300

    Usando middleware para tratamento de erro

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,22 +2,21 @@ const knex = require('../database');
 
 module.exports = {
 
-    async List(req, res) {
+    async List(req, res, next) {
         try {
             const results = await knex('users');
             return res.json(results);
-            
         } catch (error) {
-            return res.status(500).json({'MESSAGE': error})
+            next(error);
         }
     },
 
-    async Create(req, res) {
+    async Create(req, res, next) {
         try {
             await knex('users').insert({...req.body});
             return res.status(201).json();
         } catch (error) {
-            return res.status(500).json({'MESSAGE': error})
+            next(error);
         }
     }
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ app.use(express.json());
 
 app.use(require('./routes'));
 
+//catch all
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({error: error.message});
+});
+
 app.listen(8080, () => console.log('The server is running!'));
